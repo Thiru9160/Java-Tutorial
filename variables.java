0f4130afd86ff7c1f36e964b600809a2ab7eefcf
Namani Thirumalesh <namanithirumalesh9160@gmail.com>
2024-10-27 12:39:52 +0530

    jdk,jre,jvm

---
+++ variables.java
@@ -0,0 +1,27 @@
+/**
+ * variables
+ */
+public class variables {
+
+     public static void main(String[] args) {
+        //this single line comment
+/* this is 
+ * multi line comment
+ */
+//java variables are different types like string,numbres,float 
+//syntax: type variablename= value;
+String name="samandsidd";
+System.out.println(name);
+System.out.println("hello"+name);
+final int num=15;
+System.out.println(num);
+char letter='s';
+int a,b,c;
+a=b=c=10;
+System.out.println(a);
+System.out.println(b);
+System.out.println(c);
+System.out.println(letter);
+System.out.println();
+     }
+}
